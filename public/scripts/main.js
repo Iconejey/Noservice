85d36ed2078266a2f04cfd3a6c49b75cd45ef0a1
Nicolas Gouwy <43481095+Iconejey@users.noreply.github.com>
2024-10-28 15:41:17 +0000

    Moved front auth logic into the html file

--- public/scripts/main.js
+++
@@ -1,4 +0,0 @@
-// Get the origin app name from search params
-const url_params = new URLSearchParams(location.search);
-const origin_app = url_params.get('app');
-$('#origin-app').innerText = origin_app;
