62e408921bc5ef67361c89e41c3dace0af6527ff
Nicolas Gouwy <43481095+Iconejey@users.noreply.github.com>
2025-02-17 14:22:18 +0000

    Refactor AI model handling to support multiple models and improve error management

--- src/ai.js
+++ src/ai.js
@@ -5,27 +5,46 @@ class AI {
 		// Initialize the model
 		this.genAI = new GoogleGenerativeAI(api_key);
 
-		this.prefered_model = 'gemini-2.0-flash-thinking-exp-01-21';
-		this.backup_model = 'gemini-2.0-flash-lite-preview-02-05';
-		this.last_model_error_time = null;
+		this.models = [
+			{ name: 'gemini-2.0-flash-thinking-exp-01-21', last_error_time: 0 },
+			{ name: 'gemini-2.0-flash-lite-preview-02-05', last_error_time: 0 },
+			{ name: 'gemini-2.0-flash-exp', last_error_time: 0 },
+			{ name: 'gemini-1.5-flash', last_error_time: 0 }
+		];
+	}
+
+	// Get the model first in the list that has not errored in the last 2 minutes
+	get prefered_model() {
+		return this.models.find(model => Date.now() - model.last_error_time > 60000 * 2);
 	}
 
 	// Generate text content
 	async generate(opt) {
 		// Determine the model to use
 		let model = this.prefered_model;
-		if (this.last_model_error_time && Date.now() - this.last_model_error_time < 60000 * 2) {
-			model = this.backup_model;
+		if (!model) throw new Error('All models are in error state');
+		console.log('Using model:', model.name);
+
+		try {
+			// Create a generative model
+			this.model = this.genAI.getGenerativeModel({ systemInstruction: opt.system, model: model.name });
+
+			// Start generation
+			const result = await this.model.generateContentStream(opt.prompt);
+			return result.stream;
+		} catch (error) {
+			// If 503 error, try another model
+			if (error.status === 503) {
+				model.last_error_time = Date.now();
+				console.error(
+					'503 error, trying another model',
+					this.models.map(m => ({ name: m.name, minutes_since_error: (Date.now() - m.last_error_time) / 60000 }))
+				);
+				return this.generate(opt);
+			}
+
+			console.error(error);
 		}
-
-		console.log('Using model:', model);
-
-		// Create a generative model
-		this.model = this.genAI.getGenerativeModel({ systemInstruction: opt.system, model });
-
-		// Start generation
-		const result = await this.model.generateContentStream(opt.prompt);
-		return result.stream;
 	}
 
 	signalError() {
